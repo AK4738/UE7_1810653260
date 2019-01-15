34b39a342da869c78f0fc798ebf19de06257c030
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2019-01-15 15:52:17 +0100

    Aufgabe 2

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,17 +1,16 @@
 import javax.swing.*;
 
 public class Aufgabe1 {
-    public static void main(String[]args){
-        String x = JOptionPane.showInputDialog("Geben Sie eine Zahl ein: ") ;
-        int y = 1;
-        while(y==1) {
-            if (x.matches("1")) {
-                y = 2;
-                System.out.println("basd");
+    public static void main(String[] args) {
+        eingabe();
+    }
+        public static void eingabe () {
+            try {
+                Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl ein: "));
+            } catch (NumberFormatException e) {
+                eingabe();
+
             }
         }
-
-
-
     }
-}
+
